9bc0c2b1e7465c33c11725aea9cb17ca9e659ae0
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-05-07 17:06:24 -0300

    updated app-directives component

---
+++ src/app/app-directives/app-directives.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-directives',
+  imports: [],
+  templateUrl: './app-directives.component.html',
+  styleUrl: './app-directives.component.scss',
+})
+export class AppDirectivesComponent {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { AppDirectivesComponent } from './app-directives/app-directives.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DataBindingComponent],
+  imports: [RouterOutlet, AppDirectivesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
