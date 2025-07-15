a8c22bd0b21e43a26530d1192bd2e49c445a3495
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-07-15 20:37:58 -0300

    finished tdfs

--- src/app/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { User } from '../user';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-template-driven-form',
-  imports: [FormsModule],
+  imports: [FormsModule, JsonPipe],
   templateUrl: './template-driven-form.component.html',
   styleUrl: './template-driven-form.component.scss'
 })
-export class TemplateDrivenFormComponent {
- firstName : string = '';
- lastName : string = '';
+export class TemplateDrivenFormComponent implements OnInit {
+  userObject : User = {};
+
+  onSubmit(userForm: NgForm) {
+    console.log(userForm.value)
+  }
+
+  ngOnInit(): void {
+   // this.userObject = {
+   //   firstName : "Rahul",
+   //   lastName : "Varma",
+   //   email : "gaims@gmail.com",
+   //   pass : 12345,
+   //   isCheck : true,
+   // }
+  }
+
+  setValues(userForm:NgForm) {
+    let obj = {
+      firstName : "Rahul",
+      lastName : "Varma",
+      email : "gaims@gmail.com",
+      pass : 12345,
+      isCheck : true,
+    }
+    userForm.setValue(obj);
+  }
+
+  patchValues(userForm:NgForm) {
+    let obj = {
+       firstName : "Rahul",
+       lastName : "Varma",
+       email : "gaims@gmail.com",
+    }
+    userForm.control.patchValue(obj);
+  }
+
+  resetValues(userForm:NgForm) {
+    userForm.reset();
+    // userForm.resetForm();
+  }
 }
