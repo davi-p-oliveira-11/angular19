645a40ff69f09c4974e00087d463bc24ab2da01c
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-07-16 18:20:27 -0300

    added reactive forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { AttributeDirectivesComponentComponent } from './attribute-directives-co
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { BuiltInPipesComponent } from './built-in-pipes/built-in-pipes.component';
 import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 export const routes: Routes = [
       // {path: '', component: UserComponent},
@@ -16,5 +17,6 @@ export const routes: Routes = [
       {path : 'attribute-directive', component: AttributeDirectivesComponentComponent },
       {path : 'built-in-pipes', component: BuiltInPipesComponent},
       {path : 'tdf', component: TemplateDrivenFormComponent},
+      {path : 'rf', component: ReactiveFormComponent},
       {path : '**', component: PageNotFoundComponent}
 ];
