f46e619683ec8d8af68af0a7ea19a695065072ca
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-07-18 15:41:27 -0300

    view-user

--- src/app/crud/crud.component.ts
+++ src/app/crud/crud.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Iuser } from '../iuser';
+import { CrudService } from '../crud.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-crud',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './crud.component.html',
   styleUrl: './crud.component.scss'
 })
-export class CrudComponent {
+export class CrudComponent implements OnInit {
+  
+  apiData : Iuser[] = [];
 
+  constructor(private crud : CrudService) {}
+  
+  ngOnInit(): void {
+    this.getAllData();
+  }
+
+
+  getAllData() {
+    this.crud.getData().subscribe(res =>{
+       this.apiData = res;
+    })
+  }
 }
